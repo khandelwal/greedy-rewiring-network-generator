617929a1ccd6228174aaa6467fe38d4433337c0e
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-12-25 17:02:37 +0000

    Remove semi-colons

--- undirectedgraph.py
+++ undirectedgraph.py
@@ -1,149 +1,149 @@
 #!/usr/bin/python
 
-from array import array;
+from array import array
 
 class UndirectedGraph:
     "An undirected graph"
 
     def __init__(self, inputGraphFileName):
-        self.adjList = {};
-        self.populateGraph(inputGraphFileName);
+        self.adjList = {}
+        self.populateGraph(inputGraphFileName)
 
     def populateGraph(self, graphFile):
         "Populate the graph data structure from a file"
 
-        f = open(graphFile);
+        f = open(graphFile)
         for line in f:
-            line = line.split(":", 1);
-            node = int(line[0]);
+            line = line.split(":", 1)
+            node = int(line[0])
 
-            neighborList = line[1:][0];
-            neighborList = neighborList.replace("\n", '');
-            neighborList = neighborList.split(",");
-            neighborList = filter(self.filterInts, neighborList);
-            neighborList = map(self.convertToInt, neighborList);
+            neighborList = line[1:][0]
+            neighborList = neighborList.replace("\n", '')
+            neighborList = neighborList.split(",")
+            neighborList = filter(self.filterInts, neighborList)
+            neighborList = map(self.convertToInt, neighborList)
             
             #Later we might have to write a more complicated 
             #parser, since this assumes that the file is well formed. 
             #i.e. if (1,2) is in the file, then (2,1) is also in the file. 
-            neighborArray = array('l', neighborList);
-            self.adjList[node] = neighborArray;
+            neighborArray = array('l', neighborList)
+            self.adjList[node] = neighborArray
 
     def writeDegrees(self, fileName):
         "Write out the degree for each node"
 
-        #degFile = open(fileName,"w");
-        degFile = open(fileName,'a');
+        #degFile = open(fileName,"w")
+        degFile = open(fileName,'a')
         for node in self.adjList:
-            #degFile.write(repr(node) + " " + repr(self.getNodeDegree(node)) + "\n");
-            degFile.write(repr(self.getNodeDegree(node)) + " ");
+            #degFile.write(repr(node) + " " + repr(self.getNodeDegree(node)) + "\n")
+            degFile.write(repr(self.getNodeDegree(node)) + " ")
 
-            degFile.write("\n");
-        degFile.close();
+            degFile.write("\n")
+        degFile.close()
 
     def writeDegreeDist(self, fileName):
         "Write out the degree distribution to a file"
-        degDistFile = open(fileName, "w");
+        degDistFile = open(fileName, "w")
 
-        degreeDist = {};
+        degreeDist = {}
         #Go through graph, building the degree distribution 
         for node in self.adjList:
-            degree = self.getNodeDegree(node);
+            degree = self.getNodeDegree(node)
 
             if degree in degreeDist:
-                degreeDist[degree] += 1;
+                degreeDist[degree] += 1
             else:
-                degreeDist[degree] = 1;
+                degreeDist[degree] = 1
 
         #print out the degree distribution
         for x in degreeDist.items():
-            degDistFile.write(repr(x[0]) + " " + repr(x[1]) + "\n");
+            degDistFile.write(repr(x[0]) + " " + repr(x[1]) + "\n")
 
     def writeGraph(self, fileName):
         "Write the graph to fileName"
-        f = open(fileName, 'w');
+        f = open(fileName, 'w')
 
-        f.write("digraph G {");
+        f.write("digraph G {")
 
         for node in self.adjList:
-            neighborArray = map(repr, self.adjList[node]);
+            neighborArray = map(repr, self.adjList[node])
             for neighbor in neighborArray:
-                f.write(repr(node) + "->" + neighbor + ";\n");
-            #neighbors = ",".join(neighborArray);
-            #f.write(repr(node) + ": " + neighbors + "\n");
+                f.write(repr(node) + "->" + neighbor + ";\n")
+            #neighbors = ",".join(neighborArray)
+            #f.write(repr(node) + ": " + neighbors + "\n")
 
-        f.write("}");
+        f.write("}")
     def printGraph(self):
         "Print out the graph"
         
         for node in self.adjList:
-            neighborArray = map(repr, self.adjList[node]);
-            neighbors = ",".join(neighborArray);
-            print repr(node) + ": " + neighbors;
+            neighborArray = map(repr, self.adjList[node])
+            neighbors = ",".join(neighborArray)
+            print repr(node) + ": " + neighbors
 
     def getNodeDegree(self, node):
-        return array.buffer_info(self.adjList[node])[1];
+        return array.buffer_info(self.adjList[node])[1]
 
     def getNeighborDegreeList(self, node):
         "For node, get a list of (neighbor, degree) tuples"
-        neighborList = self.adjList[node];
-        degreeTuples = map(self.getNodeDegreeTuple, neighborList);
-        return degreeTuples;
+        neighborList = self.adjList[node]
+        degreeTuples = map(self.getNodeDegreeTuple, neighborList)
+        return degreeTuples
 
     def getNodeDegreeListForWholeGraph(self):
         "For the entire graph get the (node, degree) tuples"
-        listOfAllNodes = self.getListOfNodes();
-        degreeTuples = map(self.getNodeDegreeTuple, listOfAllNodes);
-        return degreeTuples;
+        listOfAllNodes = self.getListOfNodes()
+        degreeTuples = map(self.getNodeDegreeTuple, listOfAllNodes)
+        return degreeTuples
 
 
     def getNodeDegreeTuple(self, node):
         "Return (node, degree)"
-        return (node, self.getNodeDegree(node));
+        return (node, self.getNodeDegree(node))
 
     def getListOfNodes(self):
         "Return a list of all the nodes in the graph"
-        return self.adjList.keys();
+        return self.adjList.keys()
 
     def addEdge(self, x, y):
-        xList = self.adjList[x];
+        xList = self.adjList[x]
         #Don't add an edge that's already there. 
         #Don't add an edge to itself. 
         if (xList.count(y)< 1) and (x != y):
-            xList.append(y);
-            yList = self.adjList[y];
-            yList.append(x);
+            xList.append(y)
+            yList = self.adjList[y]
+            yList.append(x)
 
-            self.adjList[x] = xList;
-            self.adjList[y] = yList;
-            return True;
+            self.adjList[x] = xList
+            self.adjList[y] = yList
+            return True
         else:
-            return False;
+            return False
 
     def removeEdge(self, x, y):
-        xList = self.adjList[x];
+        xList = self.adjList[x]
         if (xList.count(y) == 1):
-            xList.remove(y);
-            yList = self.adjList[y];
-            yList.remove(x);
+            xList.remove(y)
+            yList = self.adjList[y]
+            yList.remove(x)
 
-            self.adjList[x] = xList;
-            self.adjList[y] = yList;
-            return True;
+            self.adjList[x] = xList
+            self.adjList[y] = yList
+            return True
         else:
-            return False;
+            return False
 
     #PRIVATE METHODS 
     def filterInts(self, x):
         "Remove non-integers from a list of strings"
-        x = x.strip();
+        x = x.strip()
         if x.isdigit():
-            return True;
+            return True
         else:
-            return False;
+            return False
 
     def convertToInt(self, x):
         "Convert string to integer, stripping whitespace first"
-        x = x.strip();
-        return int(x);
+        x = x.strip()
+        return int(x)
 
